b215cc29150c8f6a6be5066631ab28f198e42c64
penicillin0 <ayahitonakamura@gmail.com>
2025-01-03 23:58:41 +0900

    SupabaseのURLと匿名キーを更新

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,7 +3,7 @@ import { Database } from './database.types';
 
 // low level securityが適切に設定されていればこれらの値は、公開して問題ない
 // @see: https://supabase.com/docs/guides/api/api-keys
-const supabaseUrl = "https://xhkclgasyrexbwmvqxag.supabase.co";
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhoa2NsZ2FzeXJleGJ3bXZxeGFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4Nzg3NDEsImV4cCI6MjA1MTQ1NDc0MX0.mKsAzD_-cxNtSYqV_TpLY6wAh2iXVrHkbahsOx8_7RM';
+const supabaseUrl = "https://cvauzkhsxmqcaledoifq.supabase.co";
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2YXV6a2hzeG1xY2FsZWRvaWZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU5MTQ0NzMsImV4cCI6MjA1MTQ5MDQ3M30.yb8X21eaR4ZVL_i0qTG1misUmYyWzWm31p4CI9JtW94';
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
