094369a82554fd72272f18ac0add9e9cf3dcb885
penicillin0 <ayahitonakamura@gmail.com>
2025-01-03 16:33:25 +0900

    supabase.tsでSupabaseのURLと匿名キーをハードコーディング

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './database.types';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// low level securityが適切に設定されていればこれらの値は、公開して問題ない
+// @see: https://supabase.com/docs/guides/api/api-keys
+const supabaseUrl = "https://xhkclgasyrexbwmvqxag.supabase.co";
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhoa2NsZ2FzeXJleGJ3bXZxeGFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4Nzg3NDEsImV4cCI6MjA1MTQ1NDc0MX0.mKsAzD_-cxNtSYqV_TpLY6wAh2iXVrHkbahsOx8_7RM';
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
